cde519c6081a3760428a69a67d7d5900b0215847
jiyun0524 <49427140+jiyun0524@users.noreply.github.com>
2021-01-05 02:07:00 +0900

    20210105

---
+++ baekjoon_10818.py
@@ -0,0 +1,15 @@
+N = int(input())
+A = list(map(int, input().split( )))
+
+# print('{} {}'.format(min(A),max(A)))
+print(min(A), max(A))
+
+# min = A[0]
+# max = A[-1]
+
+# for i in range(1, N) :
+#     if A[i] < min :
+#         min = A[i]
+#     if A[i] > max :
+#         max = A[i]
+# print(min, max)
---
+++ baekjoon_1110.py
@@ -0,0 +1,41 @@
+first = str(input()) # 기존 첫번째 값
+middle = 0 # n번째 값
+cycle = 0 # 몇 번 돌아야 같은 값이 나오는지
+last = 0
+
+first = str(first)
+
+while middle<100 :
+    for i in range(True) :
+        middle += int(first[i])
+
+        if (middle<10) :
+            next = str(middle)
+            next = next.zfill(2)
+
+        last = first[-1] + next[-1]
+        print(middle, last)
+
+        if (last == first) :
+            break
+            print(cycle)
+        else :
+            cycle += 1
+            continue
+
+
+# for i in range(True) :
+#     middle += int(first[i])
+#     print(middle)
+
+#     if (middle<10) :
+#         next = str(middle)
+#         next = next.zfill(2)
+
+#     last = first[-1] + next[-1]
+#     print(middle, last)
+#     cycle += 1
+
+#     if (last == first) :
+#         break
+#         print(cycle)
