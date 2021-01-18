55ca2c158d83e7770ae04085bfdd0d0a9f81bf87
jiyun0524 <49427140+jiyun0524@users.noreply.github.com>
2021-01-18 11:55:39 +0900

    Update book_3_4.py

--- book_3_4.py
+++ book_3_4.py
@@ -3,27 +3,18 @@
 N, K = map(int, input().split())
 count = 0
 
-while True : # 여러번의 연산이 필요하기 때문에 반복 (반복문 1)
+while True : # 여러번의 연산이 필요하기 때문에 반복 (반복문)
     if N % K == 0 : # N이 K의 배수이면
-        while True : # 얼마나 큰 수인지 모르니까 반복 (반복문 2)
-            N //= K # N을 K로 계속나눔
-            count += 1 # 나눌 때 마다 1회씩 카운트 추가
-            if N == K : # 돌리다가 N이 K랑 같은 수가 되면 나눴을 때 어짜피 1이니까
-                count += 1 # 나눈 셈 치고 1 더하기
-                break # (반복문 2) 종료
-            else :
-                N -= 1
-                count += 1
-                if N == 1 :
-                    break
-        break # (반복문 1) 종료
+        N //= K # N을 K로 계속나눔
+        count += 1 # 나눌 때 마다 1회씩 카운트 추가
+        print(N, K, count)
+        if N == K : # 돌리다가 N이 K랑 같은 수가 되면 나눴을 때 어짜피 1이니까
+            count += 1 # 나눈 셈 치고 1 더하기
+            break
+        else :
+            continue
     else :
-        while True :
-            N -= 1
-            count += 1
-            if N == 1 :
-                break
-        break
+        N -= 1
+        count += 1
 
 print(count)
-
