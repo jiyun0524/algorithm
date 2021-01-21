46da52f15ed3f804cae88a767bd80dd848cacef7
jiyun0524 <49427140+jiyun0524@users.noreply.github.com>
2021-01-21 16:03:28 +0900

    20210121
    DFS

---
+++ codingTestBook/book_5_7.py
@@ -0,0 +1,12 @@
+# 5. DFS/BFS - 예제 7 인접 리스트 방식 예제
+
+graph = [ [] for _ in range(3)]
+
+graph[0].append((1,2))
+graph[0].append((3,4))
+
+graph[1].append((5,6))
+
+graph[2].append((7,8))
+
+print(graph)
