482030ae25cc0aeb491fee4ddf6bdf387b0a72b3
jiyun0524 <49427140+jiyun0524@users.noreply.github.com>
2021-01-25 22:26:42 +0900

    20210125
    1260

--- BOJ/baekjoon_1260.py
+++ BOJ/baekjoon_1260.py
@@ -2,33 +2,63 @@ from collections import deque
 
 N, M, V = map(int, input().split())
 
-graph = [[0] * (N + 1) for _ in range(N+1)]
-queue = []
+graph = [[0] * (N+1) for _ in range(N+1)]
+
+visited = [False] * (N+1)
 
 for _ in range(M) : 
     x, y = map(int,input().split())
-    queue[x][y], queue[y][x] = 1, 1
+    graph[x][y], graph[y][x] = 1, 1
 
-def dfs(graph, startDFS, visitedDFS) :
-    visitedDFS[startDFS] = True # 첫 출발 노드는 항상 먼저 방문하는거니까
-    print(startDFS, end=' ')
-    for i in graph[startDFS] :
-        if not visitedDFS[i] :
-            dfs(graph, i, visitedDFS)
+def dfs(v) :
+    visited[v] = True # 첫 출발 노드는 항상 먼저 방문하는거니까
+    print(v, end=' ')
+    for i in range(1, N+1) :
+        if not visited[i] and graph[v][i] == 1 :
+            dfs(i)
 
-def bfs(graph, startBFS, visitedBFS) :
-    visitedBFS[startBFS] = True
+def bfs(v) :
+    queue = deque([v])
+    visited[v] = False # dfs를 실행하고나면 방문처리가 돼 있기 때문에 False로.
     while queue :
-        popBFS = queue.popleft()
-        print(popBFS, end=' ')
-        for i in graph[popBFS] :
-            if not visitedBFS[i] :
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in range(1, N+1) :
+            if visited[i] and graph[v][i] == 1 :
                 queue.append(i)
-                visitedBFS[i] = True
+                visited[i] = False
 
-visitedDFS = [False] * M
-visitedBFS = [False] * M
-
-dfs(graph, V, visitedDFS)
+dfs(V)
 print()
-bfs(graph, V, visitedBFS)
+bfs(V)
+
+# N, M, V = map(int, input().split())
+# graph = [[0]*(N+1) for _ in range(N+1)] 
+# visited = [False] * (N+1) 
+
+# for _ in range(M): 
+#     a, b = map(int, input().split()) 
+#     graph[a][b] = 1 
+#     graph[b][a] = 1 
+
+# def dfs(v): 
+#     visited[v] = True 
+#     print(v, end=' ') 
+#     for i in range(1, N + 1): 
+#         if not visited[i] and graph[v][i] == 1: 
+#             dfs(i) 
+
+# def bfs(v): 
+#     queue = [v] 
+#     visited[v] = False # dfs 실행 시에 True로 방문처리 되어있기 때문에, 여기선 False로 방문처리 
+#     while queue: 
+#         v = queue.pop(0) 
+#         print(v, end=' ') 
+#         for i in range(1, N+1): 
+#             if visited[i] and graph[v][i] == 1: 
+#                 queue.append(i) 
+#                 visited[i] = False 
+
+# dfs(V) 
+# print() 
+# bfs(V)
