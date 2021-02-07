9e2cb9bce2d2ca8b1b4c02b80123220196c52d97
jiyun0524 <49427140+jiyun0524@users.noreply.github.com>
2021-02-08 01:22:40 +0900

    20210208

--- programmers/programmers_42748.py
+++ programmers/programmers_42748.py
@@ -1,9 +1,8 @@
-array = [1, 5, 2, 6, 3, 7, 4]
-commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
-
-for x,y,z in commands :
+def solution(array, commands):
     answer = []
-    newArr = array[x-1:y]
-    newArr.sort()
-    answer.append(newArr[z-1])
-    print(answer)
+    newArr = []
+    for x,y,z in commands :
+        newArr = array[x-1:y]
+        newArr.sort()
+        answer.append(newArr[z-1])
+    return answer
